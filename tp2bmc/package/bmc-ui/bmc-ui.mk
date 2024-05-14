931d9099dede568e5a652bcdb9ebacb68342fb2a
Sebastian Barrenechea <sebastian@barrenechea.cl>
2024-05-13 22:34:02 -0400

    feat: bmc-ui v3.1.0 for testing

--- tp2bmc/package/bmc-ui/bmc-ui.mk
+++ tp2bmc/package/bmc-ui/bmc-ui.mk
@@ -3,8 +3,8 @@
 # bmc-ui
 ###########################################################
 
-BMC_UI_VERSION = v3.0.0
-BMC_UI_SITE = https://github.com/turing-machines/BMC-UI/releases/download/$(BMC_UI_VERSION)
+BMC_UI_VERSION = v3.1.0
+BMC_UI_SITE = https://github.com/barrenechea/BMC-UI/releases/download/$(BMC_UI_VERSION)
 BMC_UI_LICENSE = GPL-2.0
 BMC_UI_LICENSE_FILES = LICENSE
 define BMC_UI_INSTALL_TARGET_CMDS
